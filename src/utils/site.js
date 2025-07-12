5eb990b658c541bf1ad87c8abb648b52ee7e2105
etrisarya <163295148+etrisarya@users.noreply.github.com>
2025-07-13 01:09:29 +0700

    Update site.js

--- src/utils/site.js
+++ src/utils/site.js
@@ -6,4 +6,4 @@ export const terbit = '2025-02-17T00:03:13Z';
 export const google = '94D2TQ9cD0kh4_oN8ITg4qHjKnndVOCKIdPsSXlPB4c';
 export const yandex = 'a8f2e4f80db67f46'
 
-export const index = 'dc77e16a-3255-4940-bb86-8aeaaaa02738';
+export const index = '3309bc9b-c3e3-40d9-80f6-8123eba1336e';
